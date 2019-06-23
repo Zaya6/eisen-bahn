6a9ef04b2c790a8a45453f2787fe71f96782f542
Zaya G <wolfezay@gmail.com>
2019-06-23 18:29:45 -0500

    Added basic floor gen and camera controls

---
+++ src/test/ControlCam.gd
@@ -0,0 +1,32 @@
+extends Camera2D
+
+var nzoom = 1
+var zoomTarget = 1
+var move = Vector2(0.0, 0.0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	seed(555555)
+	set_process(true)
+
+func _process(delta):
+	if Input.is_action_pressed("ui_page_up"):
+		zoomTarget +=0.1
+	if Input.is_action_pressed("ui_page_down"):
+		zoomTarget -=0.1
+	
+	if Input.is_action_pressed("ui_up"):
+		move.y -= 50 * delta
+	if Input.is_action_pressed("ui_down"):
+		move.y += 50 * delta
+	if Input.is_action_pressed("ui_right"):
+		move.x += 50 * delta
+	if Input.is_action_pressed("ui_left"):
+		move.x -= 50 * delta
+	
+	offset += move
+	move *= 0.9
+	
+	nzoom = lerp(nzoom, zoomTarget, 0.05)
+	zoom = Vector2(nzoom, nzoom)
+	
--- src/test/train.gd
+++ src/test/train.gd
@@ -6,8 +6,26 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process(true)
+	_make_layout()
+	pass
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_make_layout()
+	
+func _make_layout():
+	var grid = _make_floor_layout()
+	var defaults = grid.defaultTiles
+	var cells = grid.get_used_cells()
+	for pos in cells:
+		grid.set_cellv(pos,_rand_tile(defaults))
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _make_floor_layout():
+	return $floor
+	pass
+
+func _rand_tile(defaults):
+	return defaults[randi() % defaults.size()]
+#	var tom = Array().count(
