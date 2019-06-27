25e864d72c57ce946e4d49c2a59ca32a15a8911c
ZayaG <ZayaG@users.noreply.github.com>
2019-06-27 16:34:33 -0500

    Got the pattern matching selected only the outer frame now

--- src/test/stamping.gd
+++ src/test/stamping.gd
@@ -1,18 +1,30 @@
 extends Node2D
 
-
+var pattern = Image.new()
+var stamp = Image.new()
 
 func _ready():
-	var pattern = Image.new()
+	set_process(true)
+	
 	pattern.load("res://src/test/test_art/test_pattern1.png")
 	
-	var stamp = Image.new()
 	stamp.load("res://src/test/test_art/stamp_1.png")
 	
-	try_stamp(pattern, stamp)
 
-func try_stamp(pattern, stamp, start=[0,0]):
-	var cutout = pattern.get_rect( Rect2(start[0],start[1], stamp.get_width(), stamp.get_height()))
+var cut_pos = Vector2(0,0)
+func _process(delta):
+	
+	if cut_pos.y < pattern.get_height():
+		if cut_pos.x < pattern.get_width():
+			try_stamp(cut_pos)
+			cut_pos.x += 1
+		else:
+			cut_pos.x = 0
+			cut_pos.y += 1
+	
+
+func try_stamp(start=Vector2(0,0)):
+	var cutout = pattern.get_rect( Rect2(start.x,start.y, stamp.get_width(), stamp.get_height()))
 	
 	var px = 0
 	var py = 0
@@ -20,25 +32,19 @@ func try_stamp(pattern, stamp, start=[0,0]):
 	var height = stamp.get_height()
 	var maskRect = Rect2(1,1,width-2,height-2)
 	
+	pattern.lock()
 	while py < height:
 		while px < width:
 			
-			if maskRect.has_point(Vector2(px,py)):
-				print(px)
-				px+= 1
-				break
-			
-			#print(px)
-			pattern.lock()
-			pattern.set_pixel(px,py, Color(1,0,0,1))
-			pattern.unlock()
+			if not maskRect.has_point(Vector2(px,py)):
+				pattern.set_pixel(start.x + px,start.y + py, Color(1,0,0,1))
 			#iterate through x axis
 			px += 1
 		
 		#reset x axis iterator and iterate on y axis
 		px = 0
 		py += 1
-	
+	pattern.unlock()
 	var tex = ImageTexture.new()
 	tex.create_from_image(pattern)
 	tex.set_flags(2)
