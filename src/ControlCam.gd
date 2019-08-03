b846e74ef385c9b8f7393ae14c370e3abc59bd74
Zaya G <Zaya6@users.noreply.github.com>
2019-08-03 15:09:35 -0500

    Added loading of json stamp libraries
    it's better to just store stamps in json files

--- src/ControlCam.gd
+++ src/ControlCam.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 var nzoom = 1
-var zoomTarget = 10
+var zoomTarget = 1
 var move = Vector2(0.0, 0.0)
 
 # Called when the node enters the scene tree for the first time.
@@ -11,18 +11,18 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_page_up"):
-		zoomTarget +=0.05
-	if Input.is_action_pressed("ui_page_down"):
 		zoomTarget -=0.05
+	if Input.is_action_pressed("ui_page_down"):
+		zoomTarget +=0.05
 	
 	if Input.is_action_pressed("ui_up"):
-		move.y -= 50 * delta
+		move.y -= int(50 * delta)
 	if Input.is_action_pressed("ui_down"):
-		move.y += 50 * delta
+		move.y += int(50 * delta)
 	if Input.is_action_pressed("ui_right"):
-		move.x += 50 * delta
+		move.x += int(50 * delta)
 	if Input.is_action_pressed("ui_left"):
-		move.x -= 50 * delta
+		move.x -= int(50 * delta)
 	
 	offset += move
 	move *= 0.9
