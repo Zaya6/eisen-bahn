533ba63f174f74cdf7398da252da6f269d66d4a8
Zaya G <Zaya6@users.noreply.github.com>
2019-08-04 11:07:20 -0500

    Just got pattern matching working again
    Took a lot of work but pixel matching has now become tile matching. Lots of code just to translate indexes int x/y coord and back again. Really confusing stuff. Check comments at top for explanation of how that works

--- src/scenes/Editor.gd
+++ src/scenes/Editor.gd
@@ -4,7 +4,7 @@ extends Node2D
 # y = index / size
 # index = y * size + x
 
-var size = 6
+var size = 50
 var testlayout = []
 var layout = []
 var clock = 0.0
@@ -12,7 +12,7 @@ var clock = 0.0
 func _ready():
 	for i in range(0,size*size):
 		#layout.append(randi()%2-1)
-		layout.append(1)
+		layout.append(-1)
 		testlayout.append(i)
 	
 #	print(Stamps.stampLibraries["dungeon1.start"]["start"])
@@ -22,20 +22,21 @@ func _process(delta):
 	#for now just loop through all the stamp libraries
 	var library = Stamps.stampLibraries["dungeon1.start"]
 	clock += delta
-	if clock < 5:
+	if clock < 1000:
 		clock = 0.0
-		print ("tick")
+#		print ("tick")
 		tileMatch(library)
 		layoutToTilemap()
 
-func placeStamp(x, y, stamp, sz):
+func placeStamp(x, y, stamp, stampSize):
 	#printStamp(stamp, size)
-	for i in range(sz*sz):
-#		$TileMap.set_cell(x + i%size, y + i/size, stamp[i])
-		layout[ (y + i/sz)*sz + x] = stamp[i]
-		#print(" i" + str( size * (y + i/size) + (x + i%size )) + " =>" + str(stamp[i]))
-	printStamp(layout,size)
-	print(layout.size())
+	for i in range(stamp.size()):
+		var stampx = x + i%stampSize
+		var stampy = y + i/stampSize
+		layout[ stampy*size + stampx] = stamp[i]
+#		print(" i" + str( size * (y + i/size) + (x + i%size )) + " =>" + str(stamp[i]))
+#		print(stampSize)
+#	printStamp(layout,size)
 
 func layoutToTilemap():
 	for i in range(layout.size()):
@@ -79,16 +80,18 @@ func tileMatch(library):
 					if layout[x + y * size] == pattern[j]: #compare tiles with pattern to match
 						unlock -= 1
 						#unlock keeps track of number of matched tiles
+					else:
+						unlock = pattern.size()
 			if unlock <= 0:
-				print("---------------------placing "+ stampName +" x" + str(i%size) + ":y" + str(i/size))
+#				print("---------------------placing "+ stampName +" x" + str(i%size) + ":y" + str(i/size))
 				placeStamp(int(i%size), int(i/size), stamp["stamp"], stampSize)
 				stageUnlock -= 1
+#			print("---------------------checking "+ stampName +" x" + str(x) + ":y" + str(x))
 		
 		if stageUnlock < library.size():
 			i = 0
 			#if one has stamped, keep going
-#		else:
-#			print("done")
+#	print("#######done#######")
 
 func stampMatch(stamp):
 	#it's faster to loop through the layout once and 
@@ -110,8 +113,12 @@ func stampMatch(stamp):
 				if layout[x + y * size] == pattern[j]: #compare tiles with pattern to match
 					unlock -= 1
 					#unlock keeps track of number of matched tiles
+				else:
+					unlock = pattern.size()
 		if unlock <= 0:
 			print("---------------------placing "+ "Start" +" x" + str(i%size) + ":y" + str(i/size))
 			placeStamp(int(i%size), int(i/size), stamp["stamp"], stampSize)
+			
+#		print("---------------------checking "+ "start" +" x" + str(i%size) + ":y" + str(i/size))
 		
 
