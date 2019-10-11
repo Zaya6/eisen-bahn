7f4f38393732be7fd115493e1872e789926a0ac1
Zaya <wolfezay@gmail.com>
2019-10-11 17:50:02 -0500

    Got traincar_test is working again
    Arrays caused problems and stopped working in gdscript

--- src/levels/traincar_test.gd
+++ src/levels/traincar_test.gd
@@ -8,8 +8,12 @@ export(NodePath) var seedLabel
 
 # stamp libraries and matchers
 var libraryNames = ["train_test","train_test","train_test"]
-var libraries:Array
-var stageMatchers: Array 
+var library1 : Array
+var library2 : Array
+var library3 : Array
+var stageMatcher1:TileMatcher
+var stageMatcher2:TileMatcher
+var stageMatcher3:TileMatcher
 
 
 #internal variables
@@ -20,6 +24,9 @@ var done: bool = false
 var currentMatcher: TileMatcher
 
 func _ready():
+	#set tilemap 
+	tilemap = $TileMap
+	
 	# get tilemap object
 	if randomSeed:
 		randomize();
@@ -30,36 +37,36 @@ func _ready():
 	seed(spawnSeed)
 	
 	# load libraries
-	libraries.push_back( EB.stampLibraries[libraryNames[0]])
-	libraries.push_back( EB.stampLibraries[libraryNames[1]])
-	libraries.push_back( EB.stampLibraries[libraryNames[2]])
+	library1 = EB.stampLibraries[libraryNames[0]]
+	library2 = EB.stampLibraries[libraryNames[1]]
+	library3 = EB.stampLibraries[libraryNames[2]]
 	
 	#set up tile match generators
-	stageMatchers.push_back( TileMatcher.new(tilemap,libraries[0]))
-	stageMatchers.push_back( TileMatcher.new(tilemap,libraries[1]))
-	stageMatchers.push_back( TileMatcher.new(tilemap,libraries[2]))
-	print(TileMatcher.new(tilemap,libraries[0]))
+	stageMatcher1 = TileMatcher.new(tilemap,library1)
+	stageMatcher2 = TileMatcher.new(tilemap,library2)
+	stageMatcher3 = TileMatcher.new(tilemap,library3)
+#	print(TileMatcher.new(tilemap,libraries[0]))
 
 func _process(delta):
-	var matcher = stageMatchers[stage]
-#	print(matcher)
+	var matcher = stageMatcher1
+	print(matcher)
 	
-#	clock += delta
-#	if clock > spawnPerSecond and not done:
-#
-#		var i = 0
-#		while i < loopsPerUpdate:
-#			#gencheck stores array with data on generation
-#			var genCheck = matcher.generate()
-#
-#			if not genCheck[2]:
-#				stage += 1
-#				if stage > 0: #end on specific stage
-#					done = true
-#					print("Done yeeting")
-#
-#			clock = 0
-#			i += 1
+	clock += delta
+	if clock > spawnPerSecond and not done:
+
+		var i = 0
+		while i < loopsPerUpdate:
+			#gencheck stores array with data on generation
+			var genCheck = matcher.generate()
+
+			if genCheck[0].size.y > 50 and not genCheck[2]:
+				stage += 1
+				if stage > 0: #end on specific stage
+					done = true
+					print("Done yeeting")
+
+			clock = 0
+			i += 1
 	
 	#enter to  reload scene and regenerate
 	if Input.is_action_just_pressed("ui_accept"):
