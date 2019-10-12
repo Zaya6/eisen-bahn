a6a13141e0907e5ae56d84d106d3ad08b6cea4b8
Zaya <wolfezay@gmail.com>
2019-10-12 00:42:38 -0500

    Finished multiple traincar generation
    Traincar generation test finished. This isn't likely how generation will go when making final traincars in game. In the final game each traincar should be its own map. But this shows how multiple generators can be used to split up a level into multiple stages of generation. Change the desired length by editing finalTrainSize variable in _ready()
    
    I deleted unnecessary stamps from older versions

--- src/levels/traincar_test.gd
+++ src/levels/traincar_test.gd
@@ -7,7 +7,7 @@ export(bool) var randomSeed = false
 export(NodePath) var seedLabel
 
 # stamp libraries and matchers
-var libraryNames = ["train_test","train_test","train_test"]
+var libraryNames = ["train_test","train_test_2","train_test_3"]
 var library1 : Array
 var library2 : Array
 var library3 : Array
@@ -18,9 +18,12 @@ var stageMatcher3:TileMatcher
 
 #internal variables
 var tilemap:TileMap
-var stage : int = 0
+var stage : int = 1
 var clock:float = 0
 var done: bool = false
+var sizeCheck = 10
+var lastSize = 0
+var finalTrainSize = 0
 var currentMatcher: TileMatcher
 
 func _ready():
@@ -45,29 +48,47 @@ func _ready():
 	stageMatcher1 = TileMatcher.new(tilemap,library1)
 	stageMatcher2 = TileMatcher.new(tilemap,library2)
 	stageMatcher3 = TileMatcher.new(tilemap,library3)
-#	print(TileMatcher.new(tilemap,libraries[0]))
+	
+	sizeCheck = int(rand_range(15, 20))
+	finalTrainSize = int(rand_range(50, 100))
 
 func _process(delta):
-	var matcher = stageMatcher1
-	print(matcher)
-	
 	clock += delta
 	if clock > spawnPerSecond and not done:
-
-		var i = 0
-		while i < loopsPerUpdate:
-			#gencheck stores array with data on generation
-			var genCheck = matcher.generate()
-
-			if genCheck[0].size.y > 50 and not genCheck[2]:
-				stage += 1
-				if stage > 0: #end on specific stage
+		clock = 0
+		var matcher
+		match stage:
+			1:
+				matcher = stageMatcher1
+			2:
+				matcher = stageMatcher2
+			3: 
+				matcher = stageMatcher3
+			
+		#gencheck stores array with data on generation
+		var genCheck = matcher.generate()
+		lastSize = genCheck[0].size.y
+		
+		#check each stage
+		match stage:
+			1: # check if car is big enough, if it is start another
+				if lastSize > sizeCheck:
+					print(str(lastSize) + ":" + str(stage))
+					stage = 2
+			2: # Restart another traincar if generation hasn't reached finalTrainSize
+				if not genCheck[2]: 
+					print(str(lastSize) + ":" + str(stage))
+					if lastSize > finalTrainSize:
+						stage = 3
+					else:
+						stage = 1
+						sizeCheck = lastSize + int(rand_range(15,20))
+			3: # if finalTrainSize has been passed, put top on last car
+				if not genCheck[2]:
+					print(str(lastSize) + ":" + str(stage))
 					done = true
-					print("Done yeeting")
-
-			clock = 0
-			i += 1
-	
+					
+			
 	#enter to  reload scene and regenerate
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().reload_current_scene()
